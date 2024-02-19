e9224807f47100892a1dce41d87ad1dfe86d905e
Gonzalez Salvador <salvadorsopas@gmail.com>
2024-02-19 14:31:14 -0600

    En la f:  registrarUser se impremento el uso de bcript para poder hashear el password

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,17 +1,59 @@
+const jwt = require('jsonwebtoken') // Libreria que permite generar el token
+const bcrypt = require('bcryptjs') // libreria que permite utilizar los hashe´s
+const asyncHandler = require('express-async-handler') // manejador de rerrores
+const User = require('../models/userModel') // aqui ya estamos usando mongoose.
+
 // Registrar un usuario nuevo
-const registrarUser = (req, res) => {
-    res.status(201).json({message: 'Usuario Creado'})
-}
+const registrarUser = asyncHandler(async (req, res) => {
+    // datos obtenidos atravez del req.body
+    const {name, email, password} = req.body
+
+    if(!name || !email || !password){
+        res.status(400)
+        throw new Error('Faltan datos verificalos porfavor')
+    }
+// Verificamos si el usuario existe.
+    const userExist = await User.findOne({email})
+    // Si el usuario existe mandamos un status y un msj de que el usuario ya existe en la base de datos.
+    if(userExist){
+        res.status(400)
+        throw new Error('El usuario ya existe el DB')
+    } else {
+        // hash password
+        // creamos la salt con el metodo de bcrypt con el numero de rondas
+        const salt = await bcrypt.genSalt(10)
+        // generamos el pasword
+        const hashedPassword = await bcrypt.hash(password, salt)
+        // creamos el usuario
+        const user = await User.create({
+            name: name, // nombre del campo en mi modelo
+            email: email,
+            password: hashedPassword 
+        })
+        // checamos si se creo el usuario
+        if(user){
+            res.status(201).json({
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                admin: user.esAdmin
+            })
+        } else {
+            res.status(400)
+            throw new Error('No se pudo guardar el usuario')
+        }
+    }
+})
 
 // Usuario realizando inicio de sesión
-const loginUser = (req, res) => {
+const loginUser =asyncHandler( async(req, res) => {
     res.status(200).json({message: 'El usuario a iniciado sesión'})
-}
+})
 
 // Mostrar los datos del usuario
-const datosUser = (req, res) => {
+const datosUser = asyncHandler( async(req, res) => {
     res.status(200).json({message: 'Mis datos de usuario.'})
-}
+})
 
 module.exports = {
     registrarUser,
