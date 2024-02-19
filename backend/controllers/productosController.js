49f62a1ab3c9437a4eb63690202338c66d0b38f4
Gonzalez Salvador <salvadorsopas@gmail.com>
2024-02-18 21:58:53 -0600

    C.R.U.D section complete, fully functional with the MONGODB.

--- backend/controllers/productosController.js
+++ backend/controllers/productosController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler')
 const Producto = require('../models/productoModel')
+const { default: mongoose } = require('mongoose')
 
 // Definimos las funciones que se van a ejecutar al mandar llamar una ruta.
 
@@ -31,13 +32,40 @@ const crearProducto = asyncHandler( async (req, res)=> {
 })
 
 // Funcion para manejas la solicitud UPDATE
+// Metodo para buscar y actualizar un documento del schemaProducto en la base de datos por su ID.
 const updateProducto = asyncHandler( async (req, res)=>{
-    res.status(200).json({message: `Se a modificado el producto con id: ${req.params.id}`})
+
+// Extrae el ID del producto de los parametros de la solicitud
+    const productoId = req.params.id;
+
+// verificamos si el ID del producto es un objeto valido proporcionado por 'mongoo .... ctId' 
+    if(!mongoose.Types.ObjectId.isValid(productoId)) {
+// de no ser asi devuelve un codigo de estado 
+        res.status(400)
+        throw new Error('Id del producto invalido')
+    }
+// cvon el metodo 'findIdAndUpdate' busca y actualiza el producto en la base de datos y que se requiere recibir el documento actualizado
+    const updatedProducto = await Producto.findByIdAndUpdate(productoId, req.body, {new:true})
+// Devuelve el producto actualizado como respuesta de un 200
+    
+    res.status(200).json(updatedProducto)
+   
 })
 
 // Funcion para manejar la solicitud DELETE.
 const deleteProducto = asyncHandler( async(req,res)=>{
-    res.status(200).json({message:`Se a eliminado el producto con id: ${req.params.id}`})
+
+    const producto = await Producto.findById(req.params.id)
+
+    if(!producto){
+        res.status(400)
+        throw new Error("Producto no encontrado")
+    }
+    await Producto.deleteOne(producto)
+        res.status(200).json( `El producto con ID: ${req.params.id} a sido eliminado`)
+
+    /*const deleted = await Producto.findByIdAndDelete(req.params.id, {new: true})
+    res.status(200).json(deleted)*/
 })
 
 // Exportamos las cuatro funciones, cada funcion maneja una operacion especificaen una coleccion de usuarios en una APIRESTful
